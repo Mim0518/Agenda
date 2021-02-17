5be15a25310c140a76d6ce197d812bd8b51c24ab
Mim0518 <morenorivera1234@gmail.com>
2021-02-17 09:31:42 -0600

    Primer commit

---
+++ Agenda.java
@@ -0,0 +1,11 @@
+/*
+Guillermo Moreno Rivera y Luis Fernando Chávez Jiménez
+17/February/2021
+*/
+import java.util.Scanner;
+public class Agenda{
+    public static void main (String []args){
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
+}
